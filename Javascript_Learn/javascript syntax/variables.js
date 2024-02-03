0c88aedb8b3f056ae88f6419711bcb0fc86cd543
Manoj <mr.salet13@gmail.com>
2024-02-03 16:14:12 +0530

    modyfiying variable js file

--- Javascript_Learn/javascript syntax/variables.js
+++ Javascript_Learn/javascript syntax/variables.js
@@ -1,4 +1,4 @@
-console.log("Variable demo");
+console.log("Variable demos");
 //global variable
 let Name = "Apple";
 
@@ -11,3 +11,34 @@ function MyFunction(){
 }
 
 MyFunction();
+console.log("---------------------------------")
+console.log("Var keyword demos");
+var a = "Hello Geeks"
+var b = 10;
+var c = 12;
+var d = b + c;
+ 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log("---------------------------------")
+console.log("let keyword demos");
+let x = "Hello learners"
+let y = "joining";
+let z = " 12";
+let s = y + z;
+ 
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(s);
+console.log("---------------------------------")
+
+console.log("Const keyword demo");
+const pi = 3.14;
+
+console.log(pi);
+console.log("---------------------------------")
+
