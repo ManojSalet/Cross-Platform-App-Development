f95365b8245ede833f338df8a44ef9b819b8c6d8
Manoj <mr.salet13@gmail.com>
2024-02-07 22:30:45 +0530

    create simple object in the obj.js file

--- Javascript_Learn/javasctipt objects/obj.js
+++ Javascript_Learn/javasctipt objects/obj.js
@@ -3,3 +3,24 @@ console.log("simple Object")
 const o = new Object();
 o.foo = 42;
 console.log(o);
+
+console.log("=================================");
+console.log("=================================");
+
+console.log("simple object 2");
+
+let school = {
+    name : 'Vivekananda School',
+    location : 'Delhi',
+    established : '1971',
+    20 : 1000,
+    displayInfo : function(){
+        console.log(`${school.name} was established in ${school.established} at ${school.location}`);
+
+        console.log("objects properties");
+        console.log(`The value of the key 20 is ${school['20']}`);
+    }
+}
+
+school.displayInfo();
+
