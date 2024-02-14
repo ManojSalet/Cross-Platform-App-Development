20312bd50db5f476562be1729cfcc7a8e6d1cf53
Manoj <mr.salet13@gmail.com>
2024-02-14 22:48:24 +0530

    add new folder and inside that file for learn functions

---
+++ Javascript_Learn/Function in Javascript/function.js
@@ -0,0 +1,11 @@
+console.log("Function in Javascript");
+console.log("-------------------------");
+
+console.log("basic funtion");
+
+function myFunc(g1,g2){
+    return g1 / g2;
+}
+
+const value = myFunc(8,2);
+console.log(value);
